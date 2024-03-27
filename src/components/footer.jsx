fd2db26933c78daf3282a9ddd8867458cf584635
ultralazyy <135165310+ultralazyy@users.noreply.github.com>
2024-03-27 13:46:01 +0300

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+function Footer(){
+    return <footer className="page-footer
+    #3949ab light-green-accent-3">
+    <div className="footer-copyright">
+      <div className="container">
+        ©  {new Date().getFullYear()}
+        Sharapov Kirill
+        <a className="grey-text
+        text-lighten-4 right" href="#!">Repository</a>
+      </div>
+    </div>
+  </footer>
+}
+
+export { Footer };
